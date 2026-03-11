69309a2aabb96d642ea10f228be5066586cadb90
poorvimalavade3005-collab <poorvimalavade3005@gmail.com>
2026-03-11 16:19:31 +0530

    Create Jenkinsfile.java

---
+++ Jenkinsfile.java
@@ -0,0 +1,11 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                bat 'javac Hello.java'
+                bat 'java Hello'
+            }
+        }
+    }
+}
